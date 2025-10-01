046f394a87f5f449fe4d1dc2ed6ddf37e524fdb2
dram789 <dram@wustl.edu>
2025-10-01 15:31:56 -0500

    created group flag!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,18 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.2);
+
+		double[] xArray = {0.4, 0.5, 0.6, 0.5};
+		double[] yArray = {0.5, 0.6, 0.5, 0.4};
+
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
+
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledPolygon(xArray, yArray);
+
 		
 	}
 }
